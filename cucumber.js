010e348f4a387ef7bb396ce1b4cbeb074e5e0dc8
Siddhesh Deshpande <siddheshdeshpande47@gmail.com>
2026-02-23 22:28:49 +0530

    add cucumber allure formatter output for ci report generation

--- cucumber.js
+++ cucumber.js
@@ -3,6 +3,9 @@ module.exports = {
     requireModule: ["ts-node/register"],
     require: ["tests/support/*.ts", "tests/steps/*.ts"],
     paths: ["tests/features/*.feature"],
-    format: ["progress"]
+    format: ["progress", "allure-cucumberjs/reporter"],
+    formatOptions: {
+      resultsDir: "allure-results"
+    }
   }
 };
